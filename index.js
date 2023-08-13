ad1aec5978e10d6cf1c10c7eb21f4a4006142d33
Aniqa <aanika.chowdhury04@gmail.com>
2023-08-13 20:54:28 +0600

    add data to books collection

--- index.js
+++ index.js
@@ -33,11 +33,12 @@ async function run() {
     const authorsCollection = client.db('chapter-and-verse').collection('authors');
     const publishersCollection = client.db('chapter-and-verse').collection('publishers');
     const booksCollection = client.db('chapter-and-verse').collection('books');
+    const categoriesCollection = client.db('chapter-and-verse').collection('categories');
     //----------------------------------------x------------------------------------------
 
 
 
-    //Authors' Collection----------------------------------------------------------------
+    //Authors Collection----------------------------------------------------------------
 
     //get all data
     app.get("/authors", async (req, res) => {
@@ -50,20 +51,19 @@ async function run() {
     app.get("/names-of-authors", async (req, res) => {
       const query = {};
       const options = {
-        projection: {name:1}
+        projection: { name: 1 }
       };
-      const cursor = authorsCollection.find(query,options);
+      const cursor = authorsCollection.find(query, options);
       const result = await cursor.toArray();
       res.send(result);
     })
-    
     //--------------------------------------------x----------------------------------------
 
 
 
 
-    //Publishers' collection---------------------------------------------------------------
-    
+    //Publishers collection---------------------------------------------------------------
+
     //get all data
     app.get("/publishers", async (req, res) => {
       const cursor = publishersCollection.find();
@@ -75,14 +75,64 @@ async function run() {
     app.get("/names-of-publications", async (req, res) => {
       const query = {};
       const options = {
-        projection: {name:1}
+        projection: { name: 1 }
       };
-      const cursor = publishersCollection.find(query,options);
+      const cursor = publishersCollection.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    //------------------------------------------x------------------------------------------
+
+
+
+    //Categories collection---------------------------------------------------------------
+
+    //get all data
+    app.get("/categories", async (req, res) => {
+      const cursor = categoriesCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     })
 
+    //get only id and names
+    app.get("/names-of-categories", async (req, res) => {
+      const query = {};
+      const options = {
+        projection: { name: 1 }
+      };
+      const cursor = categoriesCollection.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+    //------------------------------------------x------------------------------------------
 
+
+    //Books collection---------------------------------------------------------------
+
+    //get all data
+    app.get("/Books", async (req, res) => {
+      const cursor = booksCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    //get all data with a few info
+    app.get("/books-with-less-info", async (req, res) => {
+      const query = {};
+      const options = {
+        projection: { bookName: 1 , BookImage:1 , price:1 , availableCopies:1 , soldCopies: 1 }
+      };
+      const cursor = booksCollection.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    //add books
+    app.post("/add-books", async (req, res) => {
+      const newBook = req.body;
+      const result = await booksCollection.insertOne(newBook);
+      res.send(result);
+    })
     //------------------------------------------x------------------------------------------
 
 
